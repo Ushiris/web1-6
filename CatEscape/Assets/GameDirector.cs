c063ea18f014feaf9a5d1c5e411da760fb390930
Unknown <umiushicco@gmail.com>
2018-06-04 17:58:06 +0900

    結構ギリギリでした…

---
+++ CatEscape/Assets/GameDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject hpGauge;
+
+	// Use this for initialization
+	void Start ()
+    {
+        hpGauge = GameObject.Find("hpGauge");
+	}
+	
+	// Update is called once per frame
+	public void DecreasaHp ()
+    {
+        hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+	}
+}
